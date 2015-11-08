8ad177d7e6560c07d195bfad717ec1bdc17ae6c1
yosoyelpipi <lcondori@gmail.com>
2015-11-08 08:47:39 -0300

    Cambios

--- www/js/index.js
+++ www/js/index.js
@@ -1,71 +1,67 @@
-var existe_db;
+/* global $ */
+var ws;
 var db;
+var user;
+var pass;
 
-function OnBodyLoad(){
-	console.log("Ejecuté el onBodyLoad");	
-	document.addEventListener("deviceready", onDeviceReady, false);
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        //scanear();
+        verificarWS('configurado', 'No tenés configurado los parámetros de conexión. Andá a la sección Parámetros/Configurar WS para comenzar.');
+    }   
+};
+
+
+//Ejecuto el lector de código de barras
+function scanear(){
+    cordova.plugins.barcodeScanner.scan(
+        //Si el scaneo del barcode Scanner funciona ejecuta la función result
+        function (result) {  
+            //Guardamos el resultado del código QR o código de barras en una variable
+            var codigoQR=result.text;
+            //Introducimos esa variable en el campo 
+            $('#resultado').html(codigoQR);
+        }, 
+        //Si no, pues ejecuta la función error.
+        function (error) {
+            notificacion("Ha ocurrido un error al escanear.");
+        }
+    );
+};
+
+//Verifico si el usuario definió o no el WS
+function verificarWS(c,m){
+    
+    if ((!ws) || (!db) || (!user) || (!pass) ){
+        console.log('no definiste el ws, por lo tanto no puedo mostrar el div '+ c);
+        $('#error').show();
+        $('#mgnalert').html('<p class="lead">'+ m +'</p>');
+    }else{
+        console.log('ok si esta definido.');
+        $('#'+ c +'').show();
+    } 
 }
 
-function onDeviceReady(){
-	
-	existe_db = window.localStorage.getItem("existe_db");	
-	
-	db = window.openDatabase("ERPITRIS", "1.0", "Pedidos Offline", 200000);
-	
-	if(existe_db == null){
-	    console.log("la BD es null");
-		creaDB();
-	}else{
-		console.log("la BD está definida");
-		cargaDatos();
-	}
-
-	//Habilita la función del botón atrás.
-	document.addEventListener("backbutton", onBackKeyDown, false);	
-
-	//Habilita la función del botón menú.
-	document.addEventListener("menubutton", onMenuKeyDown, false);
-	
-	//Depuro los pedidos para migrar
-	depuraIniDatos();
-	
-}//Fin OnReadyDevice
-
-
-function codBarras(){
-	window.plugins.barcodeScanner.scan( function(result) {
-			alert("We got a barcode\n" +
-					"Result: " + result.text + "\n" +
-					"Format: " + result.format + "\n" +
-					"Cancelled: " + result.cancelled);
-		}, function(error) {
-			alert("Scanning failed: " + error);
-					}
-		);
+function genInventario(){
+    verificarWS('genInventario', 'No tenés configurado los parámetros de conexión. Andá a la sección Parámetros/Configurar WS y vuelve a intentar generar una toma de inventario.');
 }
-
-
-function creaDB(){
-	console.log('Se ejecutó crearDB');
+function tomaInventario(){
+    verificarWS('tomaInventario', 'No tenés configurado los parámetros de conexión. Andá a la sección Parámetros/Configurar WS y vuelvé toma de inventario.');
 }
-
-function cargaDatos(){
-	console.log('Se ejecutó cargarDatos');
+function depInventario(){
+    verificarWS('depInventario', 'No tenés configurado los parámetros de conexión. Andá a la sección Parámetros/Configurar WS y vuelve a intentar depurar una toma de inventario.');
 }
-
-function depuraIniDatos(){
-	console.log('Se ejecutó depurarIniDatos');
+function configWS(){
+    console.log('Muenstro la configuración de WS.');
+    //verificarWS('configInven', 'No tenés configurado los parámetros de conexión. Andá a la sección Parámetros/Configurar WS y vuelve a intentar generar una toma de inventario.');
 }
-
-function onBackKeyDown() {
-            if( confirm("Realmente desea salir de la aplicación? Para navegar por esta app utilice los enlaces internos.") )
-            {
-                  navigator.app.exitApp();
-            }
+function helpNow(){
+    console.log('Muestro la ayuda.');
 }
-
-function onMenuKeyDown(){
-	console.log('Se ejecutó depurarIniDatos');
-}
-
-/* Inicializo la función codigo de barras */
